6d86ac9c0e5823a19ea87daf94b60bbc401236f5
Jan Pomykacz <jasiek2miliony@gmail.com>
2023-05-13 14:50:47 +0200

    Finished basic warehouse

--- distributed_sales_system/warehouse.py
+++ distributed_sales_system/warehouse.py
@@ -1,6 +1,6 @@
-# from typing import List
 import json
 from distributed_sales_system import json_path
+
 
 class Product:
 
@@ -8,7 +8,6 @@ class Product:
         self.__name = name
         self.amount = amount
         self.limit = limit
-
 
     def __repr__(self) -> str:
         return f"[name: {self.__name}, amount: {self.amount}, limit: {self.limit}]"
@@ -20,42 +19,40 @@ class Warehouse:
         # initialize empty dict
         self.__products = {}
 
-
     def __repr__(self) -> str:
         return f"{self.__products}"
-    
 
     def add_product(self, product_name: str, amount: int = 0, limit: int = 10) -> None:
         with open(json_path) as f:
             json_content = json.load(f)
             if product_name not in json_content:
-                raise Exception("Product doesn't exists (not specified in product register)")
+                raise AttributeError(
+                    "Product doesn't exists (not specified in product register)")
             elif product_name in self.__products.keys():
-                raise Exception("Product already exists in warehouse!")
+                raise AttributeError("Product already exists in warehouse!")
             elif amount < 0:
                 raise ValueError("Amount cannot be less than zero!")
             else:
-                self.__products[product_name] = Product(product_name, amount, limit)
-
+                self.__products[product_name] = Product(
+                    product_name, amount, limit)
 
     def delete_product(self, product_name: str) -> None:
         if product_name in self.__products:
             del self.__products[product_name]
 
-
     def increase_amount(self, product_name: str, amount: int = 1) -> None:
         if product_name not in self.__products:
-            raise Exception("Product doesn't exists in warehouse!")
+            raise AttributeError("Product doesn't exists in warehouse!")
         if (self.__products[product_name].amount + amount) < self.__products[product_name].limit:
             self.__products[product_name].amount += amount
         else:
-            raise AttributeError(f"Cannot have more products than specified limit of {self.__products[product_name].limit}!")
+            raise AttributeError(
+                f"Cannot have more products than specified limit of {self.__products[product_name].limit}!")
 
-    
     def decrease_amount(self, product_name: str, amount: int = 1) -> None:
         if product_name not in self.__products:
-            raise Exception("Product doesn't exists in warehouse!")
+            raise AttributeError("Product doesn't exists in warehouse!")
         if self.__products[product_name].amount - amount >= 0:
             self.__products[product_name].amount -= amount
         else:
-           raise AttributeError("Cannot have less products than zero!")
+            raise AttributeError("Cannot have less products than zero!")
