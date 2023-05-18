d3b5207630a94ce636406fe01611e5be992f2f5c
justynagibas <juztynagibas@gmail.com>
2023-05-18 16:15:11 +0200

    Customer class with documentation.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from distributed_sales_system.warehouse import Warehouse
 from distributed_sales_system.customer import Customer
+from distributed_sales_system import global_user_register
 
 if __name__ == "__main__":
     # ware1 = Warehouse()
@@ -7,6 +8,12 @@ if __name__ == "__main__":
     # print(ware1)
     # ware1.decrease_amount('apple', 2)
     # print(ware1)
-    customer = Customer(name='customer')
-    customer.generate_shopping_list()
-    print('x')
+    global_user_register.add_producer('p1', ["apple", "pear", "banana"])
+    global_user_register.add_producer('p2', ["apple", "banana", "orange"])
+    global_user_register.add_producer('p3', ["apple", "watermelon", "x"])
+    customer1 = Customer(name='customer1')
+    customer1.browsing_producers_offer()
+
+
+
+
