67047f5a77adfbe6776199da61cbadfecdcffe89
Jan Pomykacz <jasiek2miliony@gmail.com>
2023-05-19 10:39:19 +0200

    Added docstrings for producer

--- distributed_sales_system/product_generator.py
+++ distributed_sales_system/product_generator.py
@@ -1,6 +1,18 @@
 from typing import List, Tuple
 
 class GeneratorProduct:
+    '''
+    Class representing product inside generator
+
+    ...
+
+    Attributes
+    ----------
+    create_time (int):
+            Time that it takes to create product.
+    create_amount (int):
+            How much product is created in one period.
+    '''
 
     def __init__(self, create_time: int, create_amount: int) -> None:
         self.create_time = create_time
@@ -11,7 +23,20 @@ class GeneratorProduct:
 
 
 class Generator:
+    '''
+    A class representing product generator.
 
+    ...
+
+    Attributes
+    ----------
+    products (dict):
+            Dictionary mapping product name to its create time and create amount.
+    default_create_time (int):
+            Default create time of given product (used if create time not passed in constructor).
+    default_create_amount (int):
+            Default create amount of given product (used if limit not passed in constructor).
+    '''
     default_create_time = 5
     default_create_amount = 1
 
@@ -31,6 +56,17 @@ class Generator:
 
 
     def add_product(self, product_name: str, create_time: int = default_create_time, create_amount: int = default_create_amount ) -> None:
+        '''
+        Method for adding new products to generator. Only to be used as part of Producer.add_product method 
+        (otherwise errors are likely to happen).
+
+            Parameters:
+                    product_name (str): Name of the product to be added.
+                    create_time (int): Time it takes to create given product.
+                    create_amount (int): How much given product is created in one period.
+            Returns:
+                    None
+        '''
         if product_name in self.products:
             raise ValueError("Product is already generated!")
         else:
@@ -38,20 +74,52 @@ class Generator:
 
 
     def delete_product(self, product_name: str) -> None:
+        '''
+        Method for removing product from generator. Only to be used as part of Producer.delete_product method 
+        (otherwise errors are likely to happen).
+
+            Parameters:
+                    product_name (str): Name of the product to be deleted.
+
+            Returns:
+                    None
+        '''
         if product_name in self.products:
             del self.products[product_name]
 
 
     def increase_create_amount(self, product_name: str, create_amount: int = 1) -> None:
+        '''
+        Method used for increasing created amount of given product in the generator.
+        If increasing more than 50 then info error will be raised.
+
+            Parameters:
+                     product_name (str): Name of the product.
+                     create_amount (int): How much created amount of product should increase.
+
+            Returns:
+                    None
+        '''
         if product_name not in self.products:
             raise ValueError("Product is not generated!")
         if (self.products[product_name].create_amount + create_amount) < 50:
             self.products[product_name].create_amount += create_amount
         else:
             raise ValueError(
-                f"Cannot produce more at a time than 50!")
+                "Cannot produce more at a time than 50!")
 
     def decrease_create_amount(self, product_name: str, create_amount: int = 1) -> None:
+        '''
+        Method used for decreasing created amount of given product in the generator.
+        If decreasing to less than zero ValueError will raise.
+
+            Parameters:
+                     product_name (str): Name of the product.
+                     create_amount (int): How much created amount of product should decrease.
+
+            Returns:
+                    None
+        '''
         if product_name not in self.products:
             raise ValueError("Product is not generated!")
         if self.products[product_name].create_amount - create_amount >= 0:
@@ -60,6 +128,18 @@ class Generator:
             raise ValueError("Cannot produce less than zero!")
 
     def change_create_time(self, product_name: str, create_time: int = 5) -> None:
+        '''
+        Method used for changing creation time of product in generator.
+        Upper and lower limit set as 0 and 1000. 
+        Raising or lowering beyond those will raise an error.
+
+            Parameters:
+                     product_name (str): Name of the product.
+                     create_time (int): New creation time for given product.
+
+            Returns:
+                    None
+        '''
         if product_name not in self.products:
             raise ValueError("Product is not generated!")
         if self.products[product_name].create_time < 0:
--- distributed_sales_system/warehouse.py
+++ distributed_sales_system/warehouse.py
@@ -3,6 +3,19 @@ from distributed_sales_system import logging
 
 class WarehouseProduct:
 
+    '''
+    Class representing product inside warehouse
+
+    ...
+
+    Attributes
+    ----------
+    amount (int):
+            Amount of this product currently stored in warehouse.
+    limit (int):
+            Maximum number of this product that warehouse can hold.
+    '''
+
     def __init__(self, amount: int, limit: int) -> None:
         self.amount = amount
         self.limit = limit
@@ -12,8 +25,21 @@ class WarehouseProduct:
 
 
 class Warehouse:
-    
-    default_amout = 5
+    '''
+    A class representing warehouse.
+
+    ...
+
+    Attributes
+    ----------
+    products (dict):
+            Dictionary mapping product name to its amount and limit.
+    default_amount (int):
+            Default amount of given product in warehouse (used if amount not passed in constructor).
+    default_limit (int):
+            Default limit of given product in warehouse (used if limit not passed in constructor).
+    '''
+    default_amount = 5
     default_limit = 10
 
     def __init__(self, products_list: List[str] |  List[Tuple[str, float, int, int, int, int]]) -> None:
@@ -21,7 +47,7 @@ class Warehouse:
         self.products = {}
         if isinstance(products_list[0], str):
             for name in products_list:
-                self.products[name] = WarehouseProduct(Warehouse.default_amout, Warehouse.default_limit)
+                self.products[name] = WarehouseProduct(Warehouse.default_amount, Warehouse.default_limit)
         else:
             for name, _, amount, limit, _, _ in products_list:
                 self.products[name] = WarehouseProduct(amount, limit)
@@ -32,7 +58,18 @@ class Warehouse:
         return f"{self.products}"
     
     
-    def add_product(self, product_name: str, amount: int = default_amout, limit: int = default_limit) -> None:
+    def add_product(self, product_name: str, amount: int = default_amount, limit: int = default_limit) -> None:
+        '''
+        Method for adding new products to warehouse. Only to be used as part of Producer.add_product method 
+        (otherwise errors are likely to happen).
+
+            Parameters:
+                    product_name (str): Name of the product to be added.
+                    amount (int): How much given product add to warehouse.
+                    limit (int): Maximum amount of given product in warehouse.
+            Returns:
+                    None
+        '''
         if product_name in self.products:
             raise ValueError("Product already exists in warehouse!")
         else:
@@ -40,11 +77,34 @@ class Warehouse:
 
 
     def delete_product(self, product_name: str) -> None:
+        '''
+        Method for removing product from warehouse. Only to be used as part of Producer.delete_product method 
+        (otherwise errors are likely to happen).
+
+            Parameters:
+                    product_name (str): Name of the product to be deleted.
+
+            Returns:
+                    None
+        '''
         if product_name in self.products:
             del self.products[product_name]
 
 
     def increase_amount(self, product_name: str, amount: int = 1) -> None:
+        '''
+        Method used for increasing amount of given product in the warehouse.
+        Used for generating products with Generator class.
+        If increasing more than limit then info message will be raised 
+        and product amount will be set to limit.
+
+            Parameters:
+                     product_name (str): Name of the product.
+                     amount (int): How much product amount should increase.
+
+            Returns:
+                    None
+        '''
         if product_name not in self.products:
             raise ValueError("Product doesn't exists in warehouse!")
         if (self.products[product_name].amount + amount) < self.products[product_name].limit:
@@ -55,6 +115,18 @@ class Warehouse:
 
 
     def decrease_amount(self, product_name: str, amount: int = 1) -> None:
+        '''
+        Method used for decreasing amount of given product in the warehouse.
+        Used for updating warehouse state after completing order.
+        If decreasing to less than zero ValueError will raise.
+
+            Parameters:
+                     product_name (str): Name of the product.
+                     amount (int): How much product amount should decrease.
+
+            Returns:
+                    None
+        '''
         if product_name not in self.products:
             raise ValueError("Product doesn't exists in warehouse!")
         if self.products[product_name].amount - amount >= 0:
@@ -63,6 +135,18 @@ class Warehouse:
             raise ValueError("Cannot have less products than zero!")
 
     def change_limit(self, product_name: str, limit: int = 10) -> None:
+        '''
+        Method used for changing limit of product in warehouse.
+        Upper and lower limit set as 0 and 1000. 
+        Raising or lowering beyond those will raise an error.
+
+            Parameters:
+                     product_name (str): Name of the product.
+                     limit (int): New limit for given product.
+
+            Returns:
+                    None
+        '''
         if product_name not in self.products:
             raise ValueError("Product doesn't exists in warehouse!")
         if self.products[product_name].limit < 0:
