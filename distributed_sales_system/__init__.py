6f909ed4957123817ae5cddc24a6d7dfa3b6714c
justynagibas <juztynagibas@gmail.com>
2023-05-19 15:27:50 +0200

    Producer-Customer integration.

--- distributed_sales_system/__init__.py
+++ distributed_sales_system/__init__.py
@@ -1,13 +1,5 @@
+from distributed_sales_system.user_register import UserRegister
+import logging
 
-from pathlib import Path
-from .user_register import UserRegister
-
-json_name = 'product_register.py'
-
-root_path = Path(__file__).parent
-json_path = list(root_path.glob(json_name))
-if json_path:
-    json_path = json_path[0]
-else:
-    raise FileNotFoundError(f"Product register named \"{json_name}\" not found. Check if file exists or name is correct.")
 global_user_register = UserRegister()
+logging.basicConfig(level=logging.DEBUG)
